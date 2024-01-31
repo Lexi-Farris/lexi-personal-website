c7eea1a7e61a5eefbf7acb1e2915840552852bbd
Lexi <mlexifarris@gmail.com>
2024-01-31 17:01:05 -0600

    added  pointer on hover

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="w-screen h-[50px] bg-blue-green-500 flex flex-row items-center gap-5 text-neutral-300 shadow-inner">
       {menu.map((menuTab, index) => (
-        <h1 key={index} className="hover:text-warm-color-200">
+        <h1 key={index} className="hover:text-warm-color-200 cursor-pointer">
           {menuTab}
         </h1>
       ))}
