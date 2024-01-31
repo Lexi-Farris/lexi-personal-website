000eb1d56ef11013aaf903b2839fd42f981f1b4c
Lexi <mlexifarris@gmail.com>
2024-01-31 14:51:08 -0600

    added color palette to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      oranges: {
+        100: "#F7B267",
+        200: "#F79D65",
+        300: "#F4845F",
+        400: "#F27059",
+        500: "#F25C54",
+      },
+      "blue-greens": {
+        100: "#90BE6D",
+        200: "#43AA8B",
+        300: "#4D908E",
+        400: "#577590",
+        500: "#277DA1",
+      },
+      "warm-colors": {
+        100: "#F94144",
+        200: "#F3722C",
+        300: "#F8961E",
+        400: "#F9844A",
+        500: "#F9C74F",
+      },
+
+      extend: {},
+    },
   },
   plugins: [],
 };
